04dc484cf8869a70b4145ccd5d4272b7ccf31b21
zhaojian770627 <zhaojian770627@163.com>
2016-07-09 15:53:41 +0800

    up java
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- sscmqpub/inter/src/private/nc/impl/inter/InterTaskManagerImpl.java
+++ sscmqpub/inter/src/private/nc/impl/inter/InterTaskManagerImpl.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import ssc.util.mq.MQLockedExeption;
+
 import nc.bs.dao.BaseDAO;
 import nc.bs.dao.DAOException;
+import nc.bs.uap.lock.PKLock;
 import nc.itf.inter.IInterTaskManager;
 import nc.jdbc.framework.SQLParameter;
 import nc.jdbc.framework.processor.ResultSetProcessor;
+import nc.vo.pub.BusinessException;
 import nc.vo.sscmq.InterTaskVO;
 
 public class InterTaskManagerImpl implements IInterTaskManager {
@@ -27,8 +31,6 @@ public class InterTaskManagerImpl implements IInterTaskManager {
 	
 	@Override
 	public InterTaskVO[] getSynTask() throws DAOException {
-		// 加锁
-		
 		// 取Ready任务，并把状态改为处理中
 		BaseDAO dao = new BaseDAO();
 		String selSql = "select taskid,tasktype,opertype,opercode,taskstatus,taskdatac,allowundo from mq_intertaskvo where taskstatus='READY' and rownum<="+Get_TASKS;
@@ -55,6 +57,9 @@ public class InterTaskManagerImpl implements IInterTaskManager {
 		
 		InterTaskVO[] aryTaskVOS = (InterTaskVO[]) dao.executeQuery(selSql, processor);
 		
+		if(aryTaskVOS==null || aryTaskVOS.length==0)
+			return null;
+		
 		// 更新任务为状态为处理中
 		String ids = "";
 		int index = 0;
@@ -73,8 +78,19 @@ public class InterTaskManagerImpl implements IInterTaskManager {
 	}
 	
 	@Override
-	public InterTaskVO[] getSynTask_RequiresNew() throws DAOException {
-		return getSynTask();
+	public InterTaskVO[] getSynTask_RequiresNew() throws BusinessException {
+		String lockkey = "InterTaskManagerImpl_getSynTask_RequiresNew";
+		boolean locked = PKLock.getInstance().acquireLock(lockkey, null, null);
+		if (!locked) {
+			throw new BusinessException(lockkey + "get lock failed");
+		}
+		try {
+			return getSynTask();
+		} finally {
+			if (locked) {
+				PKLock.getInstance().releaseLock(lockkey, null, null);
+			}
+		}
 	}
 
 	@Override
@@ -110,6 +126,45 @@ public class InterTaskManagerImpl implements IInterTaskManager {
 		SQLParameter params = new SQLParameter();
 		params.addParam(IInterTaskManager.TASK_SUCCESS);
 		params.addParam(primaryKey);
+		new BaseDAO().executeUpdate(upSql,params);
+	}
+
+	@Override
+	public List<InterTaskVO> getInterTaskByState(String taskState) throws DAOException {
+		BaseDAO dao = new BaseDAO();
+		String selSql = "select taskid,taskstatus,taskdesc,taskresult,tasktype,opertype,opercode from mq_intertaskvo where taskstatus=?";
+		ResultSetProcessor processor = new ResultSetProcessor() {
+
+			@Override
+			public Object handleResultSet(ResultSet rs) throws SQLException {
+				List<InterTaskVO> lstTask = new ArrayList<InterTaskVO>();
+				while (rs.next()) {
+					InterTaskVO tvo = new InterTaskVO();
+					tvo.setTaskid(rs.getString(1));
+					tvo.setTaskstatus(rs.getString(2));
+					tvo.setTaskdesc(rs.getString(3));
+					tvo.setTaskresult(rs.getString(4));
+					tvo.setTasktype(rs.getString(5));
+					tvo.setOpertype(rs.getString(6));
+					tvo.setOpercode(rs.getString(7));
+					lstTask.add(tvo);
+				}
+				return lstTask;
+			}
+		};
+		
+		SQLParameter params = new SQLParameter();
+		params.addParam(taskState);
+		List<InterTaskVO> lstTasks = (List<InterTaskVO>) dao.executeQuery(selSql,params, processor);
+		return lstTasks;
+	}
+
+	@Override
+	public void updateTaskToReady(String taskid) throws DAOException {
+		String upSql="update mq_intertaskvo set taskstatus=? where taskid=?";
+		SQLParameter params = new SQLParameter();
+		params.addParam(IInterTaskManager.TASK_READY);
+		params.addParam(taskid);
 		new BaseDAO().executeUpdate(upSql,params);
 	}
 }
--- sscmqpub/inter/src/public/nc/itf/inter/IInterTaskManager.java
+++ sscmqpub/inter/src/public/nc/itf/inter/IInterTaskManager.java
@@ -1,6 +1,9 @@
 package nc.itf.inter;
 
+import java.util.List;
+
 import nc.bs.dao.DAOException;
+import nc.vo.pub.BusinessException;
 import nc.vo.sscmq.InterTaskVO;
 
 public interface IInterTaskManager {
@@ -57,8 +60,9 @@ public interface IInterTaskManager {
 	 * 
 	 * @return
 	 * @throws DAOException
+	 * @throws BusinessException 
 	 */
-	InterTaskVO[] getSynTask_RequiresNew() throws DAOException;
+	InterTaskVO[] getSynTask_RequiresNew() throws BusinessException;
 
 	/**
 	 * 记录错误出错信息
@@ -76,4 +80,21 @@ public interface IInterTaskManager {
 	 * @throws DAOException 
 	 */
 	void LogSuccess(String primaryKey) throws DAOException;
+
+	/**
+	 * 得到指定状态的任务
+	 * 
+	 * @param taskProcess
+	 * @return
+	 * @throws DAOException 
+	 */
+	List<InterTaskVO> getInterTaskByState(String taskProcess) throws DAOException;
+	
+	/**
+	 * 更新任务为待处理
+	 * 
+	 * @param taskid
+	 * @throws DAOException
+	 */
+	void updateTaskToReady(String taskid) throws DAOException;
 }
